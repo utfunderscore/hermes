a8c8fc06589b36a4ee0fb0c4e01ac7cfd8379218
Connor <concon477@virginmedia.com>
2024-07-12 17:48:29 +0100

    feature: Kryo serializer implementation

---
+++ Kryo/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "org.readutf.hermes"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    api("com.esotericsoftware:kryo5:5.6.0")
+    compileOnly(project(":Core"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
