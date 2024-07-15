66c300930a93cce1960eb68c0372b5a4591463e8
Connor <concon477@virginmedia.com>
2024-07-15 21:00:18 +0100

    (1.3.1) Add a type specific listener

--- Core/src/main/kotlin/org/readutf/hermes/listeners/TypedListener.kt
+++ Core/src/main/kotlin/org/readutf/hermes/listeners/TypedListener.kt
@@ -2,8 +2,6 @@ package org.readutf.hermes.listeners
 
 import org.readutf.hermes.Packet
 
-class TypedListener<T : Packet> {
-    fun handle(packet: T) {
-        println("Received packet: $packet")
-    }
+interface TypedListener<T : Packet> {
+    fun handle(packet: T)
 }
