c4a997eab63a0dfb8a699cc0bdbacdfca02a9d74
Connor <concon477@virginmedia.com>
2024-07-15 21:39:34 +0100

    (1.3.4) Fix annotation scanner parameters

--- Netty/src/main/kotlin/org/readutf/hermes/platform/netty/NettyPacketDecoder.kt
+++ Netty/src/main/kotlin/org/readutf/hermes/platform/netty/NettyPacketDecoder.kt
@@ -1,14 +1,17 @@
 package org.readutf.hermes.platform.netty
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.ReplayingDecoder
-import org.readutf.hermes.serializer.PacketSerializer
 import org.readutf.hermes.Packet
+import org.readutf.hermes.serializer.PacketSerializer
 
 class NettyPacketDecoder(
     private val packetSerializer: PacketSerializer,
 ) : ReplayingDecoder<Packet>() {
+    private val logger = KotlinLogging.logger { }
+
     override fun decode(
         contex: ChannelHandlerContext?,
         buffer: ByteBuf,
@@ -30,7 +33,7 @@ class NettyPacketDecoder(
 
         val packetResult = packetSerializer.deserialize(bytes)
         if (packetResult.isErr) {
-            println("Failed to deserialize packet: ${packetResult.error}")
+            logger.warn { "Failed to deserialize packet: ${packetResult.error}" }
             return
         }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-
 plugins {
     kotlin("jvm") version "2.0.0"
 }
 
 group = "org.readutf.hermes"
-version = "1.3.3"
-// version = "1.3.0"
+// version = "1.3.3-SNAPSHOT-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss"))
+version = "1.3.4"
 
 repositories {
     maven { url = uri("https://reposilite.readutf.org/releases") }
