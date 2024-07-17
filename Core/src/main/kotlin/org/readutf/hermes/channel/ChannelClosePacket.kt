a7379a4ef42da0ddafd6458c2985e08e4f3466fe
Connor <concon477@virginmedia.com>
2024-07-17 18:41:19 +0100

    refactor: Rename ChannelRegisterPacket & ChannelUnregisterPacket

--- Core/src/main/kotlin/org/readutf/hermes/channel/ChannelClosePacket.kt
+++ Core/src/main/kotlin/org/readutf/hermes/channel/ChannelClosePacket.kt
@@ -2,6 +2,6 @@ package org.readutf.hermes.channel
 
 import org.readutf.hermes.Packet
 
-class ChannelUnregisterPacket<T : HermesChannel>(
+class ChannelClosePacket<T : HermesChannel>(
     val channel: T,
 ) : Packet
--- Core/src/main/kotlin/org/readutf/hermes/channel/ChannelOpenPacket.kt
+++ Core/src/main/kotlin/org/readutf/hermes/channel/ChannelOpenPacket.kt
@@ -2,6 +2,6 @@ package org.readutf.hermes.channel
 
 import org.readutf.hermes.Packet
 
-class ChannelRegisterPacket<T : HermesChannel>(
+class ChannelOpenPacket<T : HermesChannel>(
     val channel: T,
 ) : Packet
