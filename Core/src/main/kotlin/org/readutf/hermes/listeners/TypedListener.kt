b860ca3e2a582b367a6edc6f99baf8bc9f7832fc
Connor <concon477@virginmedia.com>
2024-07-17 18:31:44 +0100

    (1.3.6) Typed listener has channel support

--- Core/src/main/kotlin/org/readutf/hermes/listeners/TypedListener.kt
+++ Core/src/main/kotlin/org/readutf/hermes/listeners/TypedListener.kt
@@ -1,7 +1,11 @@
 package org.readutf.hermes.listeners
 
 import org.readutf.hermes.Packet
+import org.readutf.hermes.channel.HermesChannel
 
-interface TypedListener<T : Packet> {
-    fun handle(packet: T)
+interface TypedListener<T : Packet, U : HermesChannel> {
+    fun handle(
+        packet: T,
+        channel: U,
+    )
 }
