0898685c95795a9b51fd3a0882ee0aa883fdb1ec
Connor <concon477@virginmedia.com>
2024-07-17 18:32:59 +0100

    chore: Bump build version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-
 plugins {
     kotlin("jvm") version "2.0.0"
 }
 
 group = "org.readutf.hermes"
-version = "1.3.3-SNAPSHOT-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss"))
-// version = "1.3.5"
+// version = "1.3.3-SNAPSHOT-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss"))
+version = "1.3.6"
 
 repositories {
     maven { url = uri("https://reposilite.readutf.org/releases") }
