d8f2545b239d937a88e2303aeb68e4eb20959377
Connor <concon477@virginmedia.com>
2024-08-24 18:13:18 +0100

    test: Listener tests now reflect changes made in previous commit

--- Core/src/test/kotlin/org/readutf/hermes/listeners/ListenerManagerTest.kt
+++ Core/src/test/kotlin/org/readutf/hermes/listeners/ListenerManagerTest.kt
@@ -5,14 +5,15 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.readutf.hermes.Packet
 import org.readutf.hermes.listeners.annotation.PacketHandler
+import java.util.concurrent.Executors
 
 class ListenerManagerTest {
-    private var listenerManager: ListenerManager = ListenerManager()
+    private var listenerManager: ListenerManager = ListenerManager(Executors.newSingleThreadScheduledExecutor())
     private var logger = KotlinLogging.logger { }
 
     @BeforeEach
     fun beforeEach() {
-        listenerManager = ListenerManager()
+        listenerManager = ListenerManager(Executors.newSingleThreadScheduledExecutor())
     }
 
     @Test
