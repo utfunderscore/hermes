4ec9f12e698e77c85618aa4f4cf0f393ccc1f3d9
utfunderscore <concon477@virginmedia.com>
2024-09-27 17:34:09 +0100

    feature: Add jackson serializer module

---
+++ Jackson/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    kotlin("jvm")
+    id("java-library")
+    id("maven-publish")
+}
+
+group = "org.readutf.hermes"
+version = "1.6.4"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    api(project(":Core"))
+    api("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "utfunderscore"
+            if (version.toString().contains("SNAPSHOT")) {
+                url = uri("https://repo.readutf.org/snapshots")
+            } else {
+                url = uri("https://repo.readutf.org/releases")
+            }
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "org.readutf.hermes"
+            artifactId = "jackson"
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
