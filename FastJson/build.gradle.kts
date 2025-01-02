2c886e4d7fb08f5e309b89693701beb7311bf6d5
connor <83981186+utfunderscore@users.noreply.github.com>
2025-01-02 16:53:56 +0000

    fix: Implement publishing to maven central

--- FastJson/build.gradle.kts
+++ FastJson/build.gradle.kts
@@ -2,48 +2,8 @@ plugins {
     kotlin("jvm")
     id("maven-publish")
 }
-
-group = "org.readutf.hermes"
-
 dependencies {
     testImplementation(kotlin("test"))
     compileOnly(project(":Core"))
     compileOnly("com.alibaba:fastjson:2.0.51")
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "utfunderscore"
-            if (version.toString().contains("SNAPSHOT")) {
-                url = uri("https://repo.readutf.org/snapshots")
-            } else {
-                url = uri("https://repo.readutf.org/releases")
-            }
-            credentials(PasswordCredentials::class)
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-        }
-    }
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "org.readutf.hermes"
-            artifactId = "fastjson"
-            version = project.version.toString()
-
-            from(components["java"])
-        }
-    }
-}
-
-java {
-    withSourcesJar()
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
 }
