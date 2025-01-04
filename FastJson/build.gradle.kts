29038eac5e0fd6873d61015878f31d46f3608891
connor <83981186+utfunderscore@users.noreply.github.com>
2025-01-04 00:44:44 +0000

    chore: Update build scripts and bump version

--- FastJson/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    kotlin("jvm")
-    id("maven-publish")
-}
-dependencies {
-    testImplementation(kotlin("test"))
-    compileOnly(project(":Core"))
-    compileOnly("com.alibaba:fastjson:2.0.51")
-}
